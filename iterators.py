416d77bbdf5267669f2a9c7d77b02b224abddead
Akrom Allayarov <abdukarimovilham061@gmail.com>
2026-09-01 16:22:25 +0500

    Created a generator that prints even number till limit. And learnt context manager via with in contextmanager.py file

--- iterators.py
+++ iterators.py
@@ -8,6 +8,15 @@
 # print(next(my_iter))
 # print(next(my_iter))
 
-squares = (x**2 for x in range(1_000_000))
-print(squares)
-print(next(squares))
+# squares = (x**2 for x in range(1_000_000))
+# print(squares)
+# print(next(squares))
+
+def even_numbers(limit):
+    for i in range(2, limit, 2):
+        yield i
+    
+func = even_numbers(10)
+
+for i in func:
+    print(i)
